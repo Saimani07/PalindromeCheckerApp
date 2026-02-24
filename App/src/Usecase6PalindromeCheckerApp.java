8e5974938e42a8d6945906fc445511051a7423d9
Saimani07 <gurramsai2007@gmail.com>
2026-02-24 14:18:56 +0530

    Update Usecase6PalindromeCheckerApp.java

--- App/src/Usecase6PalindromeCheckerApp.java
+++ App/src/Usecase6PalindromeCheckerApp.java
@@ -14,7 +14,7 @@
  */
 import java.util.*;
 
-public class Usease6PalindromeCheckerApp {
+public class Usecase6PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
